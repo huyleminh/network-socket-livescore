d8aea0755dc531eca1b7eb07481b89844cfa7951
Huy Le Minh <leminhhuy.hcmus@gmail.com>
2021-04-19 21:52:07 +0700

    commit: add db functions

---
+++ src/utils/db/details/DBDetailsHandler.py
@@ -0,0 +1,47 @@
+import json
+import shortuuid as shortId
+
+class DBDetailsHandler:
+    @staticmethod
+    def getAllDetails():
+        try:
+            f= open("database/Details.json")
+            details = fread()
+            f.close()
+        except:
+            return { "status": 500 }
+
+        detailsJSON = json.loads(details)
+        return { "status": 200, "data": detailsJSON }
+
+    @staticmethod
+    def getDetailsById(idDetail, idMatch):
+        if idDetail == "" or idMatch == "":
+            return { "status": 404 }
+
+        res = DBDetailsHandler.getAllDetails()
+
+        if res["status"] == 500:
+            return { "status": 500 }
+
+        if res["status"] == 200:
+            dataDetails = res["data"]
+            for i in range(0, len(dataDetails)):
+                if dataDetails[i]["idDetail"] == idDetail and dataDetails[i]["idMatch"] == idMatch:
+                    return { "status": 200, "data": dataDetails[i] }
+
+            return { "status": 404 }
+
+    @staticmethod
+    def writeAllDetails(details):
+        with open("database/Details.json", "w") as writeFile:
+            json.dumps(details, writeFile)
+            writeFile.close()
+
+    @staticmethod
+    def createNewDetai(detailInfo):
+        """
+        detailInfo: dict type
+        - idDetail, idMatch: required
+        """
+        pass
