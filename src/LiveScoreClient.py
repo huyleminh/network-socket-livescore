706e7a2764b1bc4d655806d9cfe46295fe09c99a
Huy Le Minh <leminhhuy.hcmus@gmail.com>
2021-04-21 03:36:12 +0700

    commit: add ui login, register, logout, home

--- src/LiveScoreClient.py
+++ src/LiveScoreClient.py
@@ -1,129 +1,151 @@
-import socket
-import threading
 import json
+import math
+import socket
+import sys
+import threading
 import time
+from functools import partial
+from tkinter import *
+from tkinter import messagebox
 
-from shared.Message import Login, Response, Request
-
-IPV4 = socket.AF_INET
-TCP = socket.SOCK_STREAM
+from shared.ConstSock import ConstSock
+from shared.Message import Login, Request, Response
+from views.Login import *
+from views.Logout import *
+from views.Register import *
 
 Thread = threading.Thread
+client = socket.socket(ConstSock.IP_ADDRESS, ConstSock.PROTOCOL)
+client.connect((ConstSock.HOST_IP, ConstSock.DEFAULT_PORT))
 
-HOST = "127.0.0.1"
-PORT = 30000
-
-# Client socket
-client = socket.socket(IPV4, TCP)
-client.connect((HOST, PORT))
-
-login = False
-connected = False
-mode = 1 #Login mode(1) or Register mode(2)
-#Please change mode in source code while waiting for UI
+login = False, connected = False, denny = False, layouts = {}
 
 def receive():
-    global login, connected
+    global login, connected, denny
     try:
-        # Receive response from server
         msg =  client.recv(1024).decode("utf8")
-
         if msg == Response.EXCESS_CONNECTION: #msg indicate that there are too many connection, force close
             print("Connection denied, queue is overflow. Please try again later.")
-            client.close()
+            denny = True
         elif msg == Response.SUCCESS_CONNECTION:
             connected = True
             print("Connect successfully.")
 
         # Done: connect sucessfully
-        while connected == True:
-            # ? Try to login
+        while connected == True: # ? Try to login
             while login == False:
-                if mode == 1:
+                mode = client.recv(1024).decode("utf8") # determine accepted mode from server
+                if mode == Response.CLOSE_CONNECTION: # raise an error when chosing mode
+                    raise Exception("Chose mode failed")
+                elif mode == Request.LOGIN_MODE:
                     msg = client.recv(1024).decode("utf8")
 
                     if msg == Login.SUCCESS:
-                        print("Login successfully.")
                         login = True
+                        layouts["login"].destroy()
+                        messagebox.showinfo("Alert", "Login successfully")
                         break
                     elif msg == Login.FAILED:
-                        print("Unable to login, please try again.")
                         login = False
-                elif mode == 2:
+                        layouts["login"].destroy()
+                        messagebox.showwarning("Alert", "Unable to login, please try again.")
+                elif mode == Request.REGISTER_MODE:
                     msg = client.recv(1024).decode("utf8")
 
                     if msg == Login.SUCCESS:
-                        print("Register successfully.")
                         login = True
+                        layouts["register"].destroy()
+                        messagebox.showinfo("Alert","Register successfully.")
                         break
                     elif msg == Login.FAILED:
-                        print("Account existed.")
                         login = False
-
-            # Listen response from server
-            msg = client.recv(1024).decode("utf8")
+                        messagebox.showwarning("Alert","Account existed.")
+                        layouts["register"].destroy()
+            msg = ""
+            while True:
+                temp = client.recv(1024).decode("utf8")
+                msg += temp
+                if len(temp) != 1024:
+                    break
             if len(msg) > 0:
                 if msg == Response.CLOSE_CONNECTION:
                     break
-                print("Receive: " + msg)
+                else:
+                    print(json.loads(msg))
 
+        client.close()
+    except Exception:
+        print("Client interrupt.")
+        connected = False
         client.close()
     except:
         print("Server error detected. Press enter to close connection.")
         connected = False
         client.close()
 
-def send():
-    global login, connected, mode
-    while connected == True:
-        # ? Try to login
-        while login == False:
-            try:
-                if mode == 1: #Login mode
-                    client.send(bytes(Request.LOGIN_MODE, "utf8"))
-                    print("Input Login Info")
-                    username = input("Username: ")
-                    password = input("Password: ")
-
-                    userInfo = { "username": username, "password": password }
-                    client.send(bytes(json.dumps(userInfo), "utf8"))
-                    time.sleep(0.1)
-                    
-                if mode == 2: #Register mode
-                    client.send(bytes(Request.REGISTER_MODE, "utf8"))
-                    print("Input Register Info")
-                    username = input("Username: ")
-                    password = input("Password: ")
-
-                    userInfo = { "username": username, "password": password, "role": "client"}
-                    client.send(bytes(json.dumps(userInfo), "utf8"))
-                    time.sleep(0.1)
-            except:
-                print("Login/Register error detected.")
-                client.close()
-
-        try:
-            requestMsg = input("Enter 0 to view all matches\nEnter q to exit")
-            #requestMsg = input("Request: ")
-            if len(requestMsg) == 0:
-                continue
-
-            if connected == False:
-                break
-
-            if requestMsg == "0":
-                client.send(bytes(Request.VIEW_ALL_MATCHES, "utf8"))
-            #client.send(bytes(requestMsg, "utf8"))
-
-            if requestMsg == "q":
-                client.send(bytes(Request.CLOSE_CONNECTION, "utf8"))
-                break
-        except:
-            print("Send error.")
-            client.close()
-
-
 clientThread = Thread(target=receive)
-clientThreadSend = Thread(target=send)
 clientThread.start()
-clientThreadSend.start()
+
+def toggleHome():
+    if login == False:
+        messagebox.showwarning("Alert", "You have to login first")
+        return
+
+    homeScreen = Toplevel(mainScreen)
+    WIDTH = homeScreen.winfo_screenwidth()
+    HEIGHT = homeScreen.winfo_screenheight()
+    PADDING_LEFT = math.ceil(WIDTH / 4)
+    PADDING_TOP = math.ceil(HEIGHT / 8)
+    homeScreen.geometry(str(math.ceil(WIDTH / 2)) + "x" + str(math.ceil(HEIGHT / 2)) + "+" + str(PADDING_LEFT) + "+" + str(PADDING_TOP))
+    searchID = StringVar()
+
+    Label(homeScreen, text="Welcome client", bg="#bd46bf", height=2, font=(14), fg="#FFFFFF").grid(row=0, columnspan=2, sticky="we")
+
+    viewAllBtn = Button(homeScreen, text="View all match", height=1, width=20, command=viewAllMatch).grid(row=1, column=0, sticky="nw")
+    viewByIDBtn = Button(homeScreen, text="View match by ID", height=1, width=20, bg="#46a049", command=partial(viewMatchByID)).grid(row=2, column=0, sticky="nw")
+    searchEntry = Entry(homeScreen, textvariable=searchID).grid(sticky="we", row=2, column=1)
+
+    homeScreen.columnconfigure(1, weight=1)
+    homeScreen.rowconfigure(4, weight=1)
+
+    homeScreen.mainloop()
+
+def toggleLogout():
+    if not login:
+        messagebox.showwarning("Alert", "You are not logged in")
+    logoutProcess(client, layouts)
+
+def onCloseWindow():
+    client.send(bytes(Request.CLOSE_CONNECTION, "utf8"))
+    layouts["mainScreen"].destroy()
+
+def viewAllMatch():
+    client.send(bytes(Request.VIEW_ALL_MATCHES, "utf8"))
+
+def viewMatchByID():
+    pass
+
+mainScreen = Tk()
+mainScreen.title("Main screen")
+
+layouts["mainScreen"] = mainScreen
+
+WIDTH = mainScreen.winfo_screenwidth()
+HEIGHT = mainScreen.winfo_screenheight()
+PADDING_LEFT = math.ceil(WIDTH / 4)
+PADDING_TOP = math.ceil(HEIGHT / 8)
+mainScreen.geometry(str(math.ceil(WIDTH / 2)) + "x" + str(math.ceil(HEIGHT / 2)) + "+" + str(PADDING_LEFT) + "+" + str(PADDING_TOP))
+
+Label(mainScreen, text="Welcome to Livescore client", bg="#000000", width=110, height=2, font=(14), fg="#FF8A0B").grid(row=0, columnspan=4, sticky="we")
+
+Button(mainScreen, text="Go to home", height=2, width=30, command=toggleHome, bg="#212121", fg="#FF8A0B").grid(row=1, column=0, sticky="w")
+Button(mainScreen, text="Login", height=2, width=20, command=partial(toggleLogin, mainScreen, client, layouts), bg="#212121", fg="#FF8A0B").grid(row=1, column=1, sticky="w")
+Button(mainScreen, text="Register", height=2, width=20, command=partial(toggleRegister, mainScreen, client, layouts), bg="#212121", fg="#FF8A0B").grid(row=1, column=2, sticky="w")
+Button(mainScreen, text="Logout", height=2, width=20, command=toggleLogout, bg="#212121", fg="#FF8A0B").grid(row=1, column=3, sticky="w")
+
+mainScreen.columnconfigure(3, weight=1)
+mainScreen.rowconfigure(2, weight=1)
+mainScreen.protocol("WM_DELETE_WINDOW", onCloseWindow)
+
+if not denny: # Success connection
+    mainScreen.mainloop()
